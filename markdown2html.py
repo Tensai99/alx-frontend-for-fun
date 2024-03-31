7cc7951e3a9783b81849f4a4653cdb8154025ffb
Tensai99 <eimtingwi@gmail.com>
2024-03-31 22:15:12 +0000

    chmod

--- markdown2html.py
+++ markdown2html.py
@@ -1,92 +1,62 @@
 #!/usr/bin/python3
-""" Write a script markdown2html.py that takes an argument 2 strings:
+"""
+This is a script to convert a Markdown file to HTML.
 
-First argument is the name of the Markdown file
-Second argument is the output file name """
+Usage:
+    ./markdown2html.py [input_file] [output_file]
 
+Arguments:
+    input_file: the name of the Markdown file to be converted
+    output_file: the name of the output HTML file
+
+Example:
+    ./markdown2html.py README.md README.html
+"""
+
+import argparse
+import pathlib
 import re
-import hashlib
-import sys
-import os
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        exit(1)
-    if not os.path.exists(sys.argv[1]):
-        sys.stderr.write("Missing " + sys.argv[1] + "\n")
-        exit(1)
 
-    with open(sys.argv[1]) as r:
-        with open(sys.argv[2], 'w') as w:
-            change_status = False
-            ordered_status = False
-            paragraph = False
-            for line in r:
-                line = line.replace('**', '<b>', 1)
-                line = line.replace('**', '</b>', 1)
-                line = line.replace('__', '<em>', 1)
-                line = line.replace('__', '</em>', 1)
+def convert_md_to_html(input_file, output_file):
+    '''
+    Converts markdown file to HTML file
+    '''
+    # Read the contents of the input file
+    with open(input_file, encoding='utf-8') as f:
+        md_content = f.readlines()
 
-                md5 = re.findall(r'\[\[.+?\]\]', line)
-                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
-                if md5:
-                    line = line.replace(md5[0], hashlib.md5(
-                        md5_inside[0].encode()).hexdigest())
+    html_content = []
+    for line in md_content:
+        # Check if the line is a heading
+        match = re.match(r'(#){1,6} (.*)', line)
+        if match:
+            # Get the level of the heading
+            h_level = len(match.group(1))
+            # Get the content of the heading
+            h_content = match.group(2)
+            # Append the HTML equivalent of the heading
+            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+        else:
+            html_content.append(line)
 
-                delete_c = re.findall(r'\(\(.+?\)\)', line)
-                remove_c_inside = re.findall(r'\(\((.+?)\)\)', line)
-                if delete_c:
-                    remove_c_inside = ''.join(
-                        c for c in remove_c_inside[0] if c not in 'Cc')
-                    line = line.replace(delete_c[0], remove_c_inside)
+    # Write the HTML content to the output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.writelines(html_content)
 
-                length = len(line)
-                headings = line.lstrip('#')
-                heading_count = length - len(headings)
-                unordered = line.lstrip('-')
-                unordered_count = length - len(unordered)
-                ordered = line.lstrip('*')
-                ordered_count = length - len(ordered)
 
-                if 1 <= heading_count <= 6:
-                    line = '<h{}>'.format(
-                        heading_count) + headings.strip() + '</h{}>\n'.format(
-                        heading_count)
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser.add_argument('input_file', help='path to input markdown file')
+    parser.add_argument('output_file', help='path to output HTML file')
+    args = parser.parse_args()
 
-                if unordered_count:
-                    if not change_status:
-                        w.write('<ul>\n')
-                        change_status = True
-                    line = '<li>' + unordered.strip() + '</li>\n'
-                if change_status and not unordered_count:
-                    w.write('</ul>\n')
-                    change_status = False
+    # Check if the input file exists
+    input_path = pathlib.Path(args.input_file)
+    if not input_path.is_file():
+        print(f'Missing {input_path}', file=sys.stderr)
+        sys.exit(1)
 
-                if ordered_count:
-                    if not ordered_status:
-                        w.write('<ol>\n')
-                        ordered_status = True
-                    line = '<li>' + ordered.strip() + '</li>\n'
-                if ordered_status and not ordered_count:
-                    w.write('</ol>\n')
-                    ordered_status = False
-
-                if not (heading_count or change_status or ordered_status):
-                    if not paragraph and length > 1:
-                        w.write('<p>\n')
-                        paragraph = True
-                    elif length > 1:
-                        w.write('<br/>\n')
-                    elif paragraph:
-                        w.write('</p>\n')
-                        paragraph = False
-
-                if length > 1:
-                    w.write(line)
-
-            if ordered_status:
-                w.write('</ol>\n')
-            if paragraph:
-                w.write('</p>\n')
-    exit(0)
+    # Convert the markdown file to HTML
+    convert_md_to_html(args.input_file, args.output_file)
